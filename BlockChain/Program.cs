cba6a4409d5abb0d2f476bed10d1cffc6c26de20
cda11roud <cda11roud@P-CDA11ROUDIL>
2025-08-27 09:32:12 +0200

    Ajoutez des fichiers projet.

---
+++ BlockChain/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BlockChain
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

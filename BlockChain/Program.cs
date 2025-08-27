7007f28b17ddef842bea0f6616c4cdf4015e25ec
LouisDufourd <102533233+LouisDufourd@users.noreply.github.com>
2025-08-27 16:13:07 +0200

    Program de test de la blockchain

--- BlockChain/Program.cs
+++ BlockChain/Program.cs
@@ -4,7 +4,44 @@
     {
         static void Main(string[] args)
         {
-            
+            Blockchain blockchain = new Blockchain();
+
+            Proprietaire premierProprietaire = new Proprietaire("Nicolas", "Rondil", new DateOnly(2001,12,23));
+            Proprietaire deuxiemeProprietaire = new Proprietaire("Nicolas", "Bisti", new DateOnly(1992,05,12));
+
+            Compte premierCompte = new Compte(premierProprietaire, "FR78 5268 4785 4561", 1000);
+            Compte deuxiemeCompte = new Compte(deuxiemeProprietaire, "FR78 7893 1596 7531", 2000);
+
+            Transaction premierTransaction = new Transaction(premierCompte, deuxiemeCompte, 500);
+            Console.WriteLine(premierTransaction.Status);
+            premierTransaction.Apply();
+            Console.WriteLine(premierTransaction.Status);
+            Block premierBlock = new Block(premierTransaction);
+            Console.WriteLine();
+            blockchain.AjouterBlock(premierBlock);
+            Console.WriteLine();
+            Console.WriteLine("Affichage de la blockchain");
+            blockchain.AfficherBlockChain();
+
+            Transaction deuxiemeTransaction = new Transaction(deuxiemeCompte, premierCompte, 3000);
+            Console.WriteLine(deuxiemeTransaction.Status);
+            deuxiemeTransaction.Apply();
+            Console.WriteLine(deuxiemeTransaction.Status);
+            Block suivant = new Block(deuxiemeTransaction, premierBlock);
+            Console.WriteLine();
+            blockchain.AjouterBlock(suivant);
+            Console.WriteLine();
+            Console.WriteLine("Affichage de la blockchain");
+            blockchain.AfficherBlockChain();
+            Console.WriteLine();
+
+            Transaction troisiemeTransaction = new Transaction(deuxiemeCompte, premierCompte, 500);
+            Console.WriteLine(troisiemeTransaction.Status);
+            suivant = new Block(troisiemeTransaction, suivant);
+            blockchain.AjouterBlock(suivant);
+            Console.WriteLine();
+            Console.WriteLine("Affichage de la blockchain");
+            blockchain.AfficherBlockChain();
         }
     }
 }
